9593410b32cccb78c81c905f3d545f88bd8fd7d3
kudanai <kudanai@gmail.com>
2021-02-08 22:39:00 +0500

    initial commit

---
+++ lottienb/lottienb.py
@@ -0,0 +1,33 @@
+from IPython.display import display, HTML
+from IPython.core.magic import register_line_magic
+from html import escape
+
+
+def __loadjs__():
+    display(HTML('<script lottienb="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>'))
+
+
+@register_line_magic
+def render_json(data, width=300, height=300):
+    __loadjs__()
+    js = f'<lottie-player ' \
+         f'lottienb="{escape(data)}" ' \
+         f'background="transparent" ' \
+         f'speed="1" ' \
+         f'style="width: {width}px; height: {height}px;" ' \
+         f'loop controls autoplay>' \
+         f'</lottie-player>'
+
+    display(HTML(js))
+
+
+@register_line_magic
+def render_file(filepath, width=300, height=300):
+    with open(filepath, "r") as f:
+        s = f.read()
+        render_json(s, width=width, height=height)
+
+
+@register_line_magic
+def render_url(url, width=300, height=300):
+    render_json(url, width=width, height=height)
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.MD", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="LottieNB",
+    version="0.0.1",
+    author="kudanai",
+    author_email="kudanai@gmail.com",
+    description="Simple insertion of Lottie players into Jupyter Notebooks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LottieFiles/LottieNB",
+    packages=['lottienb', ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License ::  MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
